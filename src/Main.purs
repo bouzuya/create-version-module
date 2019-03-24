26180b2cf067b39bc069c01ecf44c8b27c59a1e8
bouzuya <m@bouzuya.net>
2019-03-24 15:06:25 +0900

    output to file

--- src/Main.purs
+++ src/Main.purs
@@ -6,10 +6,13 @@ import Prelude
 
 import Data.Array as Array
 import Data.Either as Either
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console as Console
 import Effect.Exception as Exception
 import Formatters as Formatters
+import Node.Encoding as Encoding
+import Node.FS.Sync as FS
 import Node.Process as Process
 import Options as Options
 import Version as Version
@@ -17,10 +20,14 @@ import Version as Version
 main :: Effect Unit
 main = do
   args <- map (Array.drop 2) Process.argv
-  { options } <-
+  { arguments, options } <-
     Either.either Exception.throw pure (Options.parse args)
   if options.help
     then Console.log Options.help
     else do
       version <- Version.load
-      Console.log (Formatters.format options.noComment options.language version)
+      content <-
+        pure (Formatters.format options.noComment options.language version)
+      case Array.head arguments of
+        Nothing -> Console.log content
+        Just file -> FS.writeTextFile Encoding.UTF8 file content
