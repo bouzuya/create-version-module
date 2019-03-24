b721d476cace86beab380acdea622873a01fda9d
bouzuya <m@bouzuya.net>
2019-03-24 14:22:18 +0900

    add formatters tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,9 +5,11 @@ module Test.Main
 import Prelude
 
 import Effect (Effect)
+import Test.Formatters as Formatters
 import Test.Options as Options
 import Test.Unit.Main as TestUnitMain
 
 main :: Effect Unit
 main = TestUnitMain.runTest do
+  Formatters.tests
   Options.tests
